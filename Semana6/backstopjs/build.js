ee6c240da6e8a1b1c35d609bb28807e949423b13
AlexB <a.benitezm@uniandes.edu.co>
2022-11-20 13:55:31 -0500

    Ajustes Semana 6: Ajuste BackstopJS

--- Semana6/backstopjs/build.js
+++ Semana6/backstopjs/build.js
@@ -41,34 +41,6 @@ async function getFiles(dir) {
     return lista;
 }
 
-// function ejecutarBackstop() {
-//     exec("backstop test", (error, stdout, stderr) => {
-//         if (error) {
-//             console.log(error);
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`);
-//             return;
-//         }
-//         console.log(`stdout: ${stdout}`);
-//     });
-// }
-
-// function ejecutarBackstopApprove() {
-//     exec("backstop approve", (error, stdout, stderr) => {
-//         if (error) {
-//             console.log(`error: ${error.message}`);
-//             return;
-//         }
-//         if (stderr) {
-//             console.log(`stderr: ${stderr}`);
-//             return;
-//         }
-//         console.log(`stdout: ${stdout}`);
-//     });
-// }
 async function iniciarScript() {
     let arrayv1 = await getFiles("./v1");
     let arrayv2 = await getFiles("./v2");
@@ -78,8 +50,8 @@ async function iniciarScript() {
             for (let i = 0; i < arrayv1.length; i++) {
                 let template = {
                     label: `Escenario ${i + 1} `,
-                    url: `https://raw.githubusercontent.com/cris10958/MISO4104_202215_Equipo_White/main/Semana6/10EscenariosVersionComparacion/Cypress/Ghost_e2e/cypress/screenshots/scenarios_Ghost5.18.cy.js/${arrayv2[i]}`,
-                    referenceUrl: `https://raw.githubusercontent.com/cris10958/MISO4104_202215_Equipo_White/main/Semana6/10EscenariosVersionComparacion/Cypress/Ghost_e2e/cypress/screenshots/scenarios_Ghost3.42.cy.js/${arrayv1[i]}`,
+                    url: `https://raw.githubusercontent.com/cris10958/MISO4104_202215_Equipo_White/main/Semana6/backstopjs/v2/${arrayv2[i]}`,
+                    referenceUrl: `https://raw.githubusercontent.com/cris10958/MISO4104_202215_Equipo_White/main/Semana6/backstopjs/v1/${arrayv1[i]}`,
                     readyEvent: "",
                     readySelector: "",
                     delay: 0,
@@ -103,28 +75,12 @@ async function iniciarScript() {
         }
     }, 2000);
 
-    // setTimeout(() => {
-    //     ejecutarBackstop();
-    // }, 5000);
-    // setTimeout(() => {
-    //     ejecutarBackstopApprove();
-    // }, 15000);
-
     //Crear el archivo .json
     setTimeout(() => {
-        for (let j = 0; j < scriptPruebas["scenarios"].length; j++) {
-            let url = scriptPruebas["scenarios"][j].url;
-            let urlRef = scriptPruebas["scenarios"][j].referenceUrl;
-            scriptPruebas["scenarios"][j].url = urlRef;
-            scriptPruebas["scenarios"][j].referenceUrl = url;
-        }
         let archivoJson = JSON.stringify(scriptPruebas);
         fs.writeFileSync("backstop.json", archivoJson);
     }, 21000);
 
-    // setTimeout(() => {
-    //     ejecutarBackstop();
-    // }, 27000);
 }
 
 iniciarScript();
